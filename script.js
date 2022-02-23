ffb154d15afd99c4343f96ab65e6caf449b4ce85
Martino888 <maxmartino33@gmail.com>
2022-02-23 19:42:30 +0100

    fizzbuzzdom

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ for (let i = 1; i <= 100; i++){
     item.classList.add('square');
     item.innerHTML = i;
     container.append(item);
+    if(i%15==0){
+        item.classList.add('red');
+        item.innerHTML = 'fizzbuzz' ;
+    }else if (i%3==0){
+        item.classList.add('green');
+        item.innerHTML = 'fizz';
+    }else if (i%5==0){
+        item.classList.add('orange');
+        item.innerHTML = 'buzz';
+    }
 }
